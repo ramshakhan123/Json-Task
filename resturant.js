063f1c8aa1028a4fde27421278fc9f204d784c22
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-07-13 16:43:55 +0500

    added website...

---
+++ resturant.js
@@ -0,0 +1,991 @@
+let resturant =
+{
+    name: "Kolachi",
+
+    locations: [
+
+        "Beach View Road",
+        "(Do Darya)",
+        "DHA",
+        "Phase 8",
+        "Karachi",
+        "Pakistan"
+    ],
+    rating: "4.5/5",
+    contacts: {
+        numbers:  [
+            "111-111-001",
+            "03032766571",
+            "(021) 31130965"
+        ],
+        email: "info@kfoods.com",
+        socialMedia: [
+            {
+                name: "Facebook",
+                url: "www.sdhfjs."
+            }
+        ]
+    },
+    categories:
+    [
+        "Delivery",
+        "Dine-in",
+        "Take Away",
+    ],
+    menu: {
+        steaks: [
+            {
+                name: "",
+                price: "",
+            }
+        ],
+        pizzas: [
+
+        ],
+        soups:
+        desi:
+        burgers:
+    },
+    openTimings: [
+        {
+            "day": "Friday",
+            "timings": [
+                "12am-12",
+                "30pm",
+                "6:30pm–12am"
+            ]
+        },
+        {
+            "day": "Saturday",
+            "timings": "12pm–12am"
+        },
+        {
+            "day": "Sunday",
+            "timings": [
+                "12am–12:30pm",
+                "6:30pm–12am"
+            ]
+        },
+        {
+            "day": "Monday",
+            "timings": [
+                "12am–12:30pm",
+                "6:30pm–12am"
+            ]
+        },
+        {
+            "day": "Tuesday",
+            "timings": [
+                "12am–12:30pm",
+                "6:30pm–12am"
+            ]
+        },
+        {
+            "day": "Wednesday",
+            "timings": [
+                "12am–12:30pm",
+                "6:30pm–12am"
+            ]
+        },
+        {
+            "day": "Thursday",
+            "timings": [
+                "12am–12:30pm",
+                "6:30pm–1"
+            ]
+        }
+    ],
+
+
+// let nightTimings =
+// {
+//     "day": "Friday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Saturday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Sunday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Monday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Tuesday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Wednesday",
+//     "timings": "7pm–1am"
+// },
+// {
+//     "day": "Thursday",
+//     "timings": "7pm–1am"
+// }
+
+
+let forDelivery {
+    "phone-Number": 1111-111 - 001
+}
+
+let averageCost = {
+    "Rs. 400 - 700 / Person"
+}
+
+
+let starters = {
+    "menu": "supreme Nacho",
+    "price": 430
+},
+{
+    "menu": "spicy Mexican Wings",
+    "price": 450
+},
+{
+    "menu": "honey Mustard Wings",
+    "price": 450
+},
+{
+    "menu": "lahori Finger Fish",
+    "price": 490
+},
+{
+    "menu": "cheesy Fries",
+    "price": 190
+},
+{
+    "menu": "masala Fries",
+    "price": 170
+},
+{
+    "menu": "honey Mustard Wings",
+    "price": 450
+},
+{
+    "menu": "kolachi House Prawn",
+    "price": 670
+}
+
+
+let salads = {
+    "salad": "fresh Green Salad",
+    "price": 110
+},
+{
+    "salad": "hummus",
+    "price": 140
+},
+{
+    "salad": "raita",
+    "price": 90
+}
+
+let beefSteaks = [
+    {
+        name: "kolabdgfk dfsgfdkj fdkjgkhj",
+        price: 543
+    },
+]
+
+let beefSteaks = {
+    "menu": "kolachi Speacial Steak",
+    "price": 790
+},
+{
+    "menu": "mushroom Steak",
+    "price": 790
+},
+{
+    "menu": "pepper Steak",
+    "price": 790
+},
+{
+    "menu": "tarragon Stek",
+    "price": 790
+},
+{
+    "menu": "moroccan Staek",
+    "price": 790
+},
+{
+    "menu": "kolachi Speacial Steak",
+    "price": 790
+}
+
+
+let chickenSteaks = {
+    "menu": "kolachi Special Chicken",
+    "price": 790
+},
+{
+    "menu": "mushroom Chicken",
+    "price": 790
+},
+{
+    "menu": "pepper Chicken",
+    "price": 790
+},
+{
+    "menu": "tarragon",
+    "price": 790
+},
+{
+    "menu": "moroccan",
+    "price": 790
+}
+
+
+let desiKarahis = {
+    "menu": "peshawari Karahi",
+    "requirement": [
+        "Half/Full"
+    ]
+},
+{
+    "menu": "chicken",
+    "price": 870-1390
+},
+{
+    "menu": "mutton",
+    "price": 940 - 1590
+}
+
+
+let sulemanKarahis = {
+    "menu": "chicken",
+    "price": 870 - 1390
+},
+{
+    "menu": "mutton",
+    "price": 940-1590
+}
+
+
+let handies = {
+    "menu": "makhni Handi",
+    "price": 1390
+},
+{
+    "menu": "paneer Reshmi",
+    "price": 1390
+},
+{
+    "menu": "katakat",
+    "price": 640
+},
+{
+    "menu": "brain Masala",
+    "price": 520
+}
+
+
+let italians = {
+    "menu": "spicy Chicken Alfredo",
+    "price": 550
+},
+{
+    "menu": "pomodore",
+    "price": 550
+}
+
+
+let B.B.Qs = {
+    "menu": "mutton Chops Namkeen",
+    "price": 790
+},
+{
+    "menu": "mutton Masala Chanps",
+    "price": 790
+},
+{
+    "menu": "masto Chanps ",
+    "price": 620
+},
+{
+    "menu": "red Tikka",
+    "price": 230
+},
+{
+    "menu": "afghani Botii",
+    "price": 590
+},
+{
+    "menu": "B.B.Q Prawn",
+    "price": 990
+},
+{
+    "menu": "chicken Bihari Boti",
+    "price": 640
+},
+{
+    "menu": "afghani Botii",
+    "price": 590
+},
+{
+    "menu": "chicken Bihari Boneless",
+    "price": 640
+},
+{
+    "menu": "red Tikka",
+    "price": 230
+},
+{
+    "menu": "malai Tikka",
+    "price": 260
+},
+{
+    "menu": "hunzai Kabab",
+    "price": 610
+},
+{
+    "menu": "reshmi Kabab",
+    "price": 590
+},
+{
+    "menu": "sahna Kabab",
+    "price": 690
+},
+{
+    "menu": "mutton Ribs",
+    "price": 1690
+},
+{
+    "menu": "beef Boti",
+    "price": 490
+}
+
+
+let chinese = {
+    "menu": "chicken Shashlik",
+    "price": 540
+},
+{
+    "menu": "chicken Fried Rice",
+    "price": 410
+},
+{
+    "menu": "steam Rice With Butter",
+    "price": 290
+},
+{
+    "menu": "chicken Manchurian",
+    "price": 540
+},
+{
+    "menu": "dry Beef Chilli",
+    "price": 560
+}
+
+let burgers = {
+    "menu": "royal Taxax burger",
+    "price": 665
+},
+{
+    "menu": "spicy Crispy Chicken Burger",
+    "price": 595
+}
+{
+    "menu": "jai Apeno Burger",
+    "price": 595
+}
+
+
+let seaFoods = {
+    "menu": "snapper (HEERA)",
+    "price": 1590
+},
+{
+    "menu": "pamphlet Temtation",
+    "price": 590
+},
+{
+    "menu": "B.B.Q Prawn",
+    "price": 990
+}
+
+
+let hotTandoors = {
+    "menu": "kolacji Naan",
+    "price": 60
+},
+{
+    "menu": "garlic Naan",
+    "price": 40
+},
+{
+    "menu": "rogni Naan",
+    "price": 40
+},
+{
+    "menu": "cheese Naan",
+    "price": 190
+},
+{
+    "menu": "plain Naan",
+    "price": 30
+},
+{
+    "menu": "chapati",
+    "price": 20
+},
+{
+    "menu": "puri Paratha",
+    "price": 40
+}
+}
+
+let soupss = [
+"grilled",
+"chilli Bites",
+"hot and Sour Black",
+"spicy"
+"Chicken Noodle Soup",
+"Tortilla Soup",
+"Cream Soups"
+"Chowder",
+"Slow Cooker Soup Recipes",
+"Bean and Pea Soups",
+"Beef Soup",
+"Chicken Soup",
+"noodle Soup"
+]
+
+
+
+let beverages = {
+"menu": "coke",
+"price": 40
+},
+{
+"menu": "sprite",
+"price": 40
+},
+{
+"menu": "fanta",
+"price": 40
+},
+{
+"menu": "diet Sprite",
+"price": 40
+},
+{
+"menu": "diet Coke",
+"price": 40
+},
+{
+"menu": "1.5 Litter",
+"price": 120
+},
+{
+"menu": "red Bull",
+"price": 290
+},
+{
+"menu": "mineral Water Large",
+"price": 70
+},
+{
+"menu": "mineral Water Small",
+"price": 30
+}
+
+
+let specialDishes = {
+"names": [
+    "Karahi",
+    "Malai Boti",
+    "Tikka",
+    "Sajji",
+    "Biryani",
+    " Qorma",
+    "Special Bread",
+    "Beverages",
+    "etc"
+]
+}
+
+let ramzanOffers = {
+"offer": "Iftar",
+"menu": [
+    "icecream Fruit Chat",
+    "channa Chat",
+    "chickenChat",
+    "french Fries",
+    "aloo Samosa",
+    "keema Samosa",
+    "pakora",
+    "jalebi",
+    "gu;ab Jamun",
+    "dhaka Fish",
+    "dahi Bare",
+    "mini Sandwitch",
+    "stuffes Chillies",
+    "dates"
+]
+},
+{
+"offfer": "dinner",
+"menu": [
+    "cream of Chicken Soup",
+    "hot and Sour Soup",
+    "prawn Tempura",
+    "chicken Peshawari Karahi",
+    "deal Makhni Handi",
+    "paneer Reshmi Handi",
+    "makhni Handi",
+    "mutton Kunna",
+    "haleem",
+    "nihari",
+    "mutton Qeema",
+    "biryani",
+    "chicken Bihari Boti",
+    "masto Kabab",
+    "kofta Mutton Kabab",
+    "alferado Pasta",
+    "pepper Steak",
+    "chicken Manchorian",
+    "bbeaf Chillin Dry",
+    "fried Rice",
+    "chowmain",
+    "roghni Naan",
+    "garlic Naan",
+    "plain Naan",
+    "puri Parathaaa"
+]
+},
+{
+"offer": "salad Bar",
+"menu": [
+    "salad",
+    "raita"
+]
+},
+{
+"offer": "beverages",
+"menu": [
+    "rohhafza Plain",
+    "milk",
+    "lassi",
+    "lemon",
+    "Water"
+]
+},
+{
+"offer": "desert Bar",
+"menu": [
+    "ice-cream",
+    "variety of Deserts"
+]
+},
+{
+"notOffer": [
+    "coldDrink",
+    "tea",
+    "water"
+]
+},
+let buffet + dinner = {
+"buffet": "adult",
+"price": "Rs.2299"
+},
+{
+"buffet": "under 10",
+"price": "Rs.1399",
+},
+{
+"buffet": "under 4",
+"price": "free"
+}
+
+
+let specialDiets =
+    [
+"Vegetarian",
+"Friendly",
+"Vegan Options",
+"Halal",
+"Gluten Free Options"
+]
+
+
+let meals =
+    [
+"dinner",
+"late Night"
+]
+
+
+let features =
+    [
+"Takeout",
+"Reservations",
+"Outdoor Seating",
+"Seating",
+"Parking Available",
+"Validated Parking",
+"Valet Parking",
+"Highchairs Available",
+"Wheelchair Accessible",
+"Table Service"
+]
+
+let discounts = {
+"off": "uoto 50%",
+"onCard": "bank Alfalah Cards"
+},
+{
+"off": "uoto 50%",
+"onCard": "MCB Cards"
+},
+{
+"off": "uoto 40%",
+"onCard": "habib Bank Limited Cards"
+},
+{
+"off": "uoto 40%",
+"onCard": "habib Metro Bank  Cards"
+},
+{
+"off": "uoto 40%",
+"onCard": "askari bank Cards"
+},
+{
+"off": "uoto 30%",
+"onCard": "JS Bank  Cards"
+},
+{
+"off": "uoto 25%",
+"onCard": "meezan Bank Cards"
+},
+{
+"off": "uoto 15%",
+"onCard": "united Bank Limited Cards"
+}
+
+
+
+let resturantFacilities =
+    [
+"Kids Friendly",
+"Valvet Parking",
+"Take Away",
+"Smoking Allowed",
+"Disabled Access",
+"Shisha",
+"Smoking Allowed",
+"Wifi"
+]
+
+
+let nearestLocations =
+    [
+"Airport 7",
+"Atrium Mall",
+"Saddar 6",
+"Bahadurabad 21",
+"Boat Basin 14",
+"Burns Road 7",
+"CANNT 4",
+"Clifton 65",
+"Club Road 10",
+"DHA Phase 1 4",
+"DHA Phase 2 1",
+"DHA Phase 4 2",
+"DHA Phase 5 77",
+"DHA Phase 6 37",
+"DHA Phase 7 3",
+"DHA Phase 8 8",
+"DHA Zamzama 8",
+"Do Darya 11",
+"F.B Area 8",
+"Fatima Jinnah Colony 6",
+"Garden 4",
+"Gulistan-e-Johar 9",
+"Gulshan-e-Iqbal 37",
+"Highway 2",
+"Hussainabad 2",
+"North Nazimabad 29",
+"I.I. Chundrigarh Road 8",
+"KAECHS 1",
+"Karsaz 3",
+"Korangi 3",
+"M.A.C.H.S. 8",
+"Malir 2",
+"Mereweather Road 2",
+"Nazimabad 4",
+"North Karachi 7",
+"Nursery 8",
+"Old Sabzi Mandi 1",
+"P.E.C.H.S 11",
+"Rashid Minhas Road 14",
+"S.M.C.H.S 8",
+"Saddar 12",
+"Shahrah-e-Faisal 6",
+"SITE 1",
+"Tariq Road",
+"Dhoraji Colony 1",
+"Mereweather Road 2",
+"Nazimabad 4",
+"North Karachi 7",
+"Nursery 8",
+"Old Sabzi Mandi 1",
+"P.E.C.H.S 11",
+"Rashid Minhas Road 14",
+"S.M.C.H.S 8",
+"Saddar 12",
+"Shahrah-e-Faisal 6",
+"SITE 1",
+"Tariq Road",
+"Dhoraji Colony 1",
+"M.A.C.H.S. 8",
+"Malir 2",
+"Mereweather Road 2",
+"Nazimabad 4",
+"North Karachi 7",
+"Nursery 8",
+"Old Sabzi Mandi 1",
+"P.E.C.H.S 11",
+"Rashid Minhas Road 14",
+"S.M.C.H.S 8"
+]
+
+
+let chefsDetails = {
+"name": "zakir",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "rahat",
+"age": 41,
+"contact": 0331-2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "johar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "suleman",
+"age": 45,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulshan-e-eIqbal",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "mahir",
+"age": 39,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "ali",
+"age": 38,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "taha",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "ahmed",
+"age": 36,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "zakir",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "amir",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+},
+{
+"name": "tahir",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"education": "masters",
+"salary": 40000
+}
+
+
+let waitersDetalis = {
+"name": "tahir",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "raza",
+"age": 35,
+"contact": 0331-2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulishan",
+"salary": 35000
+},
+{
+"name": "farhan",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulishan",
+"salary": 35000
+},
+{
+"name": "tahir",
+"age": 39,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "hamza",
+"age": 32,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "mayannmar",
+"salary": 35000
+},
+{
+"name": "faizan",
+"age": 36,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "ali",
+"age": 36,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "aliyan",
+"age": 40,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "shayan",
+"age": 39,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+},
+{
+"name": "hamza",
+"age": 39,
+"contact": 0331 - 2345670,
+"cnic": 40201 - 2345768 - 0,
+"address": "gulistan-e-gohar",
+"salary": 35000
+}
+
+let branches = [
+"do Darya Karachi",
+"ocean Mall Clifton Karachi"
+]
+
+let travellerTypes =
+    [
+"Families",
+"Couples",
+"Solo",
+"Business",
+"Friends"
+]
+
+let achievement = {
+"certificate": "Certificate of Excellence",
+"year": 2015 - 2019,
+"isWinner": true
+}
+
+
+[
+{
+    "name": "ramsha Ansari",
+    "experience": [
+        "ownwer/CEO",
+        "event management & marketing company"
+    ]
+}
+]
+
+
+let events = [
+{
+    "name": "Karachi Carnival",
+    "year": 2016
+},
+{
+    "name": "Winter Shopping Festival",
+    "year": 2018
+},
+{
+    "name": "Eid Exhibition",
+    "year": 2018
+},
+{
+    "name": "spring Shopping Festival",
+    "year": 2018
+},
+{
+    "name": "Spring Exhibition",
+    "year": 2018
+},
+{
+    "name": "Winter Exhibition",
+    "year": 2017
+},
+{
+    "name": "Fall Exhibition",
+    "year": 2018
+}
+]
